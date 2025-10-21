5ac512c40943d58f7f4f33ca7c69642828c36cdb
Joey Cao <160793355+cao-joey@users.noreply.github.com>
2025-10-20 21:00:31 -0400

    Update models.py
    Added code

--- models.py
+++ models.py
@@ -1,1 +1,48 @@
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
+from datetime import datetime
 
+db = SQLAlchemy()
+
+class User(UserMixin, db.Model):
+    """
+    A User 
+    """
+    __tablename__ = 'users'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    name = db.Column(db.String(50), unique=False, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password_hash = db.Column(db.String(255), nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    # One to many relationship
+    tasks = db.relationship('Task', backref='owner', lazy=True, cascade='all, delete-orphan')
+    
+    def set_password(self, password):
+        # Hash password before storing
+        self.password_hash = generate_password_hash(password)
+    
+    def check_password(self, password):
+        # Check against hashed password
+        return check_password_hash(self.password_hash, password)
+    
+    def __repr__(self):
+        return f'<User {self.username}>'
+
+
+class Task(db.Model):
+    """
+    A Task
+    """
+    __tablename__ = 'tasks'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.String(200), nullable=False)
+    done = db.Column(db.Boolean, default=False, nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    
+    def __repr__(self):
+        return f'<Task {self.id}: {self.text[:20]}...>'
